04e048c5ada279e1de8cd1f62cad282771220dfb
Vlad <vsilverman@gmail.com>
2019-11-25 16:08:02 -0800

    adding circleci

--- .circleci/exit.sh
+++ .circleci/exit.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This script will halt circleci pipeline
+# based on the context of commit message
+
 commitmessage=$(git log -n 1)
 if [[ $commitmessage = *"[circle skip]"* ]] || [[ $commitmessage = *"[skip ci]"* ]]; then
     echo "Skipping circleci build because [circle skip] found in commit message"
