cefd3f271960083f178934931fb74ac2c4fad760
Vlad <vsilverman@gmail.com>
2020-04-17 10:58:45 -0700

    updating auth process

--- my-mvn-sonar-run.sh
+++ my-mvn-sonar-run.sh
@@ -8,4 +8,4 @@ mvn sonar:sonar \
     -Dsonar.host.url=https://sonarcloud.io \
     -Dsonar.projectKey=vsilverman_jenkins-ci \
     -Dsonar.organization=vsilverman-github \
-    -Dsonar.login=905b6ea8dd4be5fa69abad2e24a59a0b9c43130a
+    -Dsonar.login=`cat ~/SNR_LOGIN_TOKEN.txt`
