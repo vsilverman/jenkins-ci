9367c0967f9f6d3ce824ccd109c316d1ff93f9a8
Vlad <vsilverman@gmail.com>
2020-06-30 18:28:00 -0700

    updated theme config

--- scripts/update-theme.sh
+++ scripts/update-theme.sh
@@ -8,11 +8,15 @@ echo $((10#${cur_date}%2))
 if [ $((10#${cur_date}%2)) -eq 0 ];
     then
         echo 'date is even'
-        sed -i .bak 's/time-machine/cayman/' ${cfg_file}
+        sed 's/time-machine/cayman/' ${cfg_file} > ${cfg_file}.bak
+        # sed -i .bak 's/time-machine/cayman/' ${cfg_file}
     else
         echo 'date is odd'
-        sed -i .bak "s/cayman/time-machine/" ${cfg_file}
+        sed "s/cayman/time-machine/" ${cfg_file} > ${cfg_file}.bak
+        # sed -i .bak "s/cayman/time-machine/" ${cfg_file}
 fi
+rm ${cfg_file}
+mv ${cfg_file}.bak ${cfg_file}
 git add ${cfg_file}
 git commit -a -m "${msg}"
 git push
