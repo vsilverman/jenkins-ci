7ec08c188df2b2bb87bf558cad3daa85cda51e9c
vsilverman <vsilverman@gmail.com>
2022-08-30 14:49:52 -0700

    Enabling confgurable Jenkins port

--- docker/dkr-jen-bo-start.sh
+++ docker/dkr-jen-bo-start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+JENKINS_PORT=8080
 JENKINS_VERSION=2.346.3
 
 docker run --name jenkins-blueocean --rm --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
-  --publish 8080:8080 --publish 50000:50000 \
+  --publish ${JENKINS_PORT}:${JENKINS_PORT} --publish 50000:50000 \
   --volume jenkins-data:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
-  myjenkins-blueocean:{JENKINS_VERSION}-1
+  myjenkins-blueocean:${JENKINS_VERSION}-1
