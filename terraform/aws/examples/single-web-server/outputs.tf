c0ae04362390ce030fa24ac4f70b2ef955390a52
vsilverman <vsilverman@gmail.com>
2023-05-09 14:00:46 -0700

    Updating terraform deployment

--- terraform/aws/examples/single-web-server/outputs.tf
+++ terraform/aws/examples/single-web-server/outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "The public IP of the web server"
 }
+
+output "url" {
+  value = "http://${aws_instance.example.public_ip}:8080"
+  description = "URL of the web server"
+}
